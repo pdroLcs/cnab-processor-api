00e6559a1f4628b01cf2a7dbd7b0c3b425f5d4f8
Pedro Lucas <plfm3k@gmail.com>
2026-03-26 00:07:41 -0400

    Commit inicial

---
+++ src/main/java/br/com/pedro/cnab_processor_api/CnabProcessorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.cnab_processor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabProcessorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnabProcessorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedro/cnab_processor_api/CnabProcessorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.cnab_processor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabProcessorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
